6a34907c2c7dd71587fe53c55c8de0b68f0f5e55
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-03-22 10:33:48 +1300

    Initial versions of yes/no checker

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,15 @@
+# Ask the user if they have played before
+name = "Lucky Unicorns"
+show_instructions = input(f"Have you played {name} before >>> ").lower()
+
+# If they say yes, output 'Program Continues'
+if show_instructions in ("yes", "y"):
+    print("Program continues")
+
+# If they say no, output 'Display instructions'
+elif show_instructions in ("no", "n"):
+    print("Display instructions")
+
+# Otherwise, show error
+else:
+    print("Please answer 'yes' or 'no'")
